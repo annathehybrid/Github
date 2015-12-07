f1e673c542ea2c5678c319910f0a4aeca7228958
Anna the Hybrid <skateboardingalice@gmail.com>
2015-12-07 13:38:00 -0600

    play store

---
+++ HowtoCelebrateChristmas/app/src/main/java/anna/howtocelebratechristmas/Cheer_Data.java
@@ -0,0 +1,186 @@
+package anna.howtocelebratechristmas;
+
+import android.graphics.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cheer_Data {
+
+    private Cheer_Data() {
+    }
+
+    public static List<Information> load_cheer_items() {
+        List<Information> items = new ArrayList<>();
+
+        Information cheer_information_1 = new Information();
+        cheer_information_1.name = "Spend time with your loved ones ";
+        cheer_information_1.description = "On Christmas Eve and Christmas Day, spend some time with" +
+                " your family and extended family!";
+        cheer_information_1.color = Color.rgb(240, 280, 255);
+        cheer_information_1.image = "cheer_1";
+        cheer_information_1.URL = "http://brookekellyphotography.blogspot.com/2013/12/emmeline-6-months-nashville-baby.html";
+
+        items.add(cheer_information_1);
+
+        Information cheer_information_2 = new Information();
+        cheer_information_2.name = "Send a Christmas card ";
+        cheer_information_2.description = "During December, send a cute photo and a card to your " +
+                "family. They'll love it!";
+        cheer_information_2.color = Color.rgb(111, 11, 111);
+        cheer_information_2.image = "cheer_2";
+        cheer_information_2.URL = "http://photo-studio.kz/portfolio/gregory/";
+
+        items.add(cheer_information_2);
+
+
+        Information cheer_information_3 = new Information();
+        cheer_information_3.name = "Perform an act of kindness ";
+        cheer_information_3.description = "I saw on a blog someone had done 32 acts of service on their 32nd birthday. It got my mind going and I decided to involve my children in the service we would do 24 acts of service leading up to the 25th of December. We decided to do 8 a Saturday for the 3 Saturdays in December. I brainstormed ideas and wrote them down and we went to work. This became a cherished family activity in December and one that my children loved.\n" +
+                "\n" +
+                "Donated clothes and toys to Good Will\n" +
+                "\n" +
+                "Went in on a secret Santa with some other families...\n" +
+                "\n" +
+                "Gave hot chocolate to people that seemed cold. Addie walked all the way up to this man sitting by himself on a box and gave him the hot chocolate. She said \"hope this keeps you warm\" :) She was proud as can be when she came back.\n" +
+                "\n" +
+                "Got a gift card at Smiths to take to someone in the store. The girls delivered it and picked the person to give it to. We also cleaned up all the carts in the parking lot and put them where they go.\n" +
+                "\n" +
+                "Made sugar cookies for our neighbors and went carolling to them\n" +
+                "\n" +
+                "paid for someones Red Box\n" +
+                "\n" +
+                "Paid for someone's parking meter\n" +
+                "\n" +
+                "Took balloons to a hospital to be delivered to children who were there over the holidays\n" +
+                "\n" +
+                "Passed out suckers to kids at a park\n" +
+                "\n" +
+                "Took doughnuts to the Fire station and police station\n" +
+                "\n" +
+                "taped this money to a vending machine\n" +
+                "\n" +
+                "Gave spare change to Salvation Army\n" +
+                "\n" +
+                "Brought in trash cans in the neighborhood\n" +
+                "\n" +
+                "Put little notes they wrote on cars in a parking lot\n" +
+                "\n" +
+                "Leaving notes and treats in the mailbox for the mailman. She even wrote us a lovely thank you note!\n" +
+                "\n" +
+                "The kids loved giving money to a homeless person and a giving Chilis restaurant gift card to a family walking in to Chilis.\n" +
+                "\n" +
+                "Paid for the person behind us at Jack in the Box";
+        cheer_information_3.color = Color.rgb(13, 55, 125);
+        cheer_information_3.image = "cheer_3";
+        cheer_information_3.URL = "http://erikandkellyahlander.blogspot.com/2011/12/24-acts-of-service.html";
+
+        items.add(cheer_information_3);
+
+
+        Information cheer_information_4 = new Information();
+        cheer_information_4.name = "Call up and check up on a friend ";
+        cheer_information_4.description = "Not everyone is so lucky to have" +
+                " family and friends. \n\n" +
+                "Check up on your friends who may be spending their holidays alone," +
+                " and invite them to your Christmas celebrations.\n\n";
+        cheer_information_4.color = Color.rgb(222, 22, 222);
+        cheer_information_4.image = "cheer_4";
+        cheer_information_4.URL = "";
+        items.add(cheer_information_4);
+
+
+        Information cheer_information_5 = new Information();
+        cheer_information_5.name = "Read a classic Christmas story ";
+        cheer_information_5.description = "Read a novel with a Christmas theme, " +
+                "such as A Christmas Carol by Charles Dickens or " +
+                "Little Women by Lousia May Alcott.";
+        cheer_information_5.color = Color.rgb(122, 22, 222);
+        cheer_information_5.image = "cheer_5";
+        cheer_information_5.URL = "https://en.wikipedia.org/wiki/Little_Women";
+        items.add(cheer_information_5);
+
+
+
+        Information cheer_information_6 = new Information();
+        cheer_information_6.name = "Take family photos and videos ";
+        cheer_information_6.description = "Taking home photos and videos area great way to preserve " +
+                "memories!";
+        cheer_information_6.color = Color.rgb(112, 22, 222);
+        cheer_information_6.image = "cheer_6";
+        cheer_information_6.URL = "";
+        items.add(cheer_information_6);
+
+
+
+        Information cheer_information_7 = new Information();
+        cheer_information_7.name = "Call up and check up on an elderly neighbor ";
+        cheer_information_7.description = "A lot of older adults are alone at this time of year. " +
+                "Holidays are hard enough! Take care of your neighbors and " +
+                "help them out.";
+        cheer_information_7.color = Color.rgb(122, 122, 222);
+        cheer_information_7.image = "cheer_7";
+        cheer_information_7.URL = "";
+        items.add(cheer_information_7);
+
+
+
+        Information cheer_information_8 = new Information();
+        cheer_information_8.name = "Spend time with your significant other ";
+        cheer_information_8.description = "Christmas Eve Traditions for Couples\n" +
+                "\n" +
+                "    Ornaments: I know a family that does this and I LOVE it. " +
+                "This can be a tradition just for the two of you, or you can make it" +
+                " a family tradition. Each year, before you go to bed on on Christmas" +
+                " Eve, you give each other an ornament that represents a favorite memory" +
+                " together that year. It could represent a vacation, something funny, " +
+                "or something meaningful. Another way to do this is to take turns buying" +
+                " the ornament each year. After several years of doing this, you will" +
+                " have a tree full of memories, and you can have fun remembering " +
+                "them all as you pull them out each year.\n" +
+                "\t\n" +
+                "    Letters: Rather than ornaments, enjoy a festive treat just for " +
+                "the two of you (hot cocoa, sparkling cider, eggnog, etc.) and exchange " +
+                "letters expressing your favorite memory from that year. If you want to " +
+                "get creative, you can give each year a theme (funniest memory, most" +
+                " meaningful, funniest parenting moment, most admirable moment, etc.).\n" +
+                "\t\n" +
+                "    Movie in Bed. This is the last official night for Christmas " +
+                "movies, so enjoy one together just the two of you before you drift " +
+                "off. Don’t have a TV in your room? Snuggle together with your phone," +
+                " iPad or laptop. Have a special toast for the new year.\n" +
+                "\t\n" +
+                "    Kitchen Festivities. Some couples really enjoy baking together " +
+                "(not me). A night of memories involves NOT being in the kitchen; " +
+                "however, I know of several friends that have special traditions " +
+                "that involve making some type of food the night before Christmas. " +
+                "One of my friends makes doughnuts with her husband every Christmas" +
+                " Eve, and another couple I know gets in their Christmas pajamas and" +
+                " eats Chinese food together. Find a food that would be fitting for the " +
+                "two of you, and enjoy it (or make it) together every year as a tradition.\n" +
+                "\t\n" +
+                "    Predictions. This is similar to the first two traditions, " +
+                "but rather than reflecting on the previous year, you make predictions" +
+                " for the year to come. Write down where you see yourselves exactly one " +
+                "year from now, and tuck the predictions away until the next year. This " +
+                "could be funny situations you see your partner getting him or herself" +
+                " into, promotions you can see happening, silly things you can imagine " +
+                "you will fight about, or funny things your children will most likely do. " +
+                "This is a fun one to do with some sparkling cider!\n" +
+                "\n" +
+                "That’s all I’ve got- do you have any more we can add to the list? " +
+                "Leave us a comment below!\n" +
+                "\n" +
+                "I wish you all the happiest of holidays!\n" +
+                "\n";
+        cheer_information_8.color = Color.rgb(122, 225, 222);
+        cheer_information_8.image = "cheer_8";
+        cheer_information_8.URL = "http://www.somewhatsimple.com/christmas-eve-traditions-couples/";
+        items.add(cheer_information_8);
+
+
+
+        return items;
+    }
+
+}
---
+++ HowtoCelebrateChristmas/app/src/main/java/anna/howtocelebratechristmas/Decorations_Data.java
@@ -0,0 +1,207 @@
+package anna.howtocelebratechristmas;
+
+
+import android.graphics.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Decorations_Data {
+
+    private Decorations_Data() {
+    }
+
+    public static List<Information> load_decorations_items() {
+        List<Information> items = new ArrayList<>();
+
+        Information decoration_information_1 = new Information();
+        decoration_information_1.name = "Holly on the stairway banister ";
+        decoration_information_1.description = "Deck the halls, starting with the stairway banister. \n" +
+                "\tI have to use artificial greenery here because of a heat register right below the spindles. \n" +
+                "\tI tried fresh garland the first few years we were here and it’s just not worth the mess. " +
+                "\tI’ve used the same piece of garland for years. Each year I come up with" +
+                " a different way to decorate it. Last year I used giant pine cones and ribbon. " +
+                "\tThis year it’s stars and red balls.\n\n";
+        decoration_information_1.color = Color.rgb(240, 280, 255);
+        decoration_information_1.image = "decoration_1";
+        decoration_information_1.URL = "http://www.onsuttonplace.com/2013/12/deck-the-halls-christmas-entry/";
+
+        items.add(decoration_information_1);
+
+
+        Information decoration_information_2 = new Information();
+        decoration_information_2.name = "Peppermint Mason Jar Candles ";
+        decoration_information_2.description = "How to make soy peppermint candles – create handmade peppermint scented" +
+                " mason jar candles and tea lights to give to friends, teachers, neighbors, " +
+                "and co-workers this holiday season.\n\n" +
+                "Supplies:\n" +
+                "All Natural Soy Candle Wax\n" +
+                "Peppermint Oil\n" +
+                "Mason Jar\n" +
+                "Natural Candle Wick\n" +
+                "Tea Light Cups\n" +
+                "\n" +
+                "Please note: I used 1 ½ cups of candle flakes " +
+                "to make 1 half pint mason jar candle. When I used 2 cups of" +
+                " candle flakes I was able to make a mason jar candle and a" +
+                " few tea light candles. I recommend having your candlewicks ready just" +
+                " in case you have any remaining candle wax.\n" +
+                "\n" +
+                "Step one: Glue candlewick in the center of your mason jar or tea light cup.\n" +
+                "\n" +
+                "How to make soy candlesStep two: melt soy candle flakes over a double boiler or " +
+                "in microwave according to package directions. " +
+                "I like to melt mine in a large pyrex measuring cup (4 cup size)" +
+                " for easy pouring.\n" +
+                "\n" +
+                "Step three: once the wax is melted stir in peppermint oil" +
+                " (about 15-20 drops). Pour into mason jar or tea lights. Use a pencil " +
+                "to wrap the candlewick and hold in place while the candle wax hardens." +
+                " Leave candle to harden for several hours or overnight. Trim candlewick before lighting.\n" +
+                "\n" +
+                "DIY Peppermint Candles - you won't believe how easy it is to make your " +
+                "own mason jar candles! These make a great Christmas gift idea! \n\n" +
+                "Please note: the decorations on the mason jars are simply for decorative purposes. " +
+                "If you decide to decorate your candle gift I recommend using embellishments " +
+                "that can be easily removed before lighting.\n\n";
+        decoration_information_2.color = Color.rgb(240, 280, 255);
+        decoration_information_2.image = "decoration_2";
+        decoration_information_2.URL = "http://apumpkinandaprincess.com/2015/11/diy-peppermint-mason-jar-candles.html";
+
+        items.add(decoration_information_2);
+
+
+        Information decoration_information_3 = new Information();
+        decoration_information_3.name = " No Mantle Stocking Hangers ";
+        decoration_information_3.description = "We don't have a mantle, but of course still" +
+                " want to hang stockings.  So I got some wood, paint, and hooks and" +
+                " created this stocking hanger for our family of 6.\n" +
+                "\n" +
+                "I  added the fun quote with vinyl (my mom has a Cricut)  " +
+                "and some snowflakes.\n" +
+                "\n" +
+                "I just bought a pack of hooks at the hardware store and screwed" +
+                " them to the board.\n" +
+                "\n" +
+                "It is attached to the wall with some Command velcro strips so" +
+                " there will not be permanent holes in the wall after the stockings come down.\n" +
+                "\n" +
+                "I've been wanting to make some new stockings for years, " +
+                "and this is finally the year!  Can't wait to finish them and show you!\n" +
+                "\n";
+        decoration_information_3.color = Color.rgb(240, 280, 255);
+        decoration_information_3.image = "decoration_3";
+        decoration_information_3.URL = "http://listfullyblissful.blogspot.com/2012/12/no-mantle-stocking-solution.html";
+
+        items.add(decoration_information_3);
+
+
+        Information decoration_information_4 = new Information();
+        decoration_information_4.name = "Salt Dough Handprint Ornaments ";
+        decoration_information_4.description = "Time moves too fast. Let's document childhood & those precious little chubby hands. I want to capture this before these moments will soon go by.\n" +
+                "\n" +
+                "How to make Handprint Ornaments\n" +
+                "\n" +
+                "Here’s what you’ll need:\n" +
+                "\t4 cups of flour\n" +
+                "\t1 cup salt\n" +
+                "\tFood coloring\n" +
+                "\tLarge Mixing Bowl\n" +
+                "\tWater to moisten\n" +
+                "\tcookie sheet\n" +
+                "\tjust a tad of oil\n" +
+                "\ta small rolling pin or wooden dowel\n" +
+                "\ta drinking straw\n" +
+                "\tribbon\n" +
+                "\ta plate for a guide\n" +
+                "\tpaint *if desired\n" +
+                "\n" +
+                "This recipe had enough to make 3 keepsakes. Depending on the size &" +
+                " how many kids you have, you may need to double the recipe.*\n" +
+                "\n" +
+                "In your mixing bowl ~ gradually blend in your flour & salt with water.\n" +
+                "*Remember to add gradually, because you can always add more water " +
+                "then takeaway.*\n" +
+                "\n" +
+                "It was easier for me to lightly grease my baking sheet with oil & " +
+                "form & roll out on the cookie sheet. Once you have your form made, I " +
+                "suggest you use your straw to make a whole in it before you have your kids emboss their hand print.\n" +
+                "\n" +
+                "Keep in mind you need to have your dough thick enough to form the" +
+                " handprint. If your dough is too thin, it will not work. Place in a " +
+                "150* degree oven for an hour. Or you can air dry over night & for 24 hours.\n" +
+                "\n" +
+                "This truly makes my heart swoon. I love every bit of the natural & raw" +
+                " aspects of seeing their hand prints together. Making these ornaments " +
+                "has captured some beautiful moments that we are experiencing right now " +
+                "in our home. I personally feel any keepsake ornament that can mark a " +
+                "moment in time is the best kind of ornament there is. It tells a story" +
+                " & I love that about homemade ornaments, there’s nothing better.\n" +
+                "\n";
+        decoration_information_4.color = Color.rgb(240, 280, 255);
+        decoration_information_4.image = "decoration_4";
+        decoration_information_4.URL = "http://www.nestofposies-blog.com/2011/11/salt-dough-handprint-ornaments/";
+
+        items.add(decoration_information_4);
+
+
+        Information decoration_information_5 = new Information();
+        decoration_information_5.name = "Waterless Snow Globes ";
+        decoration_information_5.description = "How to make Waterless Snow Globes\n" +
+                "\n" +
+                "What you need:\n" +
+                "\n" +
+                "*you can find all of these supplies at a local craft store like" +
+                " Michaels or Hobby Lobby, or Amazon\n" +
+                "\n" +
+                "    Glass jars (varying sizes look best for a collection)\n" +
+                "    Artificial Snow (I used Snow Flakes and Twinkle Flakes)\n" +
+                "    Hot glue gun and glue\n" +
+                "    Assorted mini Christmas Trees and snowmen\n" +
+                "    Spray Adhesive (optional)\n" +
+                "    glitter (optional)\n" +
+                "    Krylon Glitter Blast (or white spray paint)\n" +
+                "\n" +
+                "1.  Remove lids and spray paint.  I used the Krylon Glitter Blast" +
+                " in silver, but white would work just as well.\n" +
+                "\n" +
+                "*optional step (not shown) – I sprayed tress and snowmen with spray" +
+                " adhesive then sprinkled with glitter\n" +
+                "\n" +
+                "2.  Hot glue your trees and snowmen to the lids (I also glued the " +
+                "canning lids together)\n" +
+                "\n" +
+                "3.  Fill jars with just a little snow.\n" +
+                "\n" +
+                "4.  Screw lids back on.\n" +
+                " \n" +
+                "Turn them over and Let It Snow!\n" +
+                "\n" +
+                "These are just too fun and easy to make – after waiting for my lids to dry," +
+                " I made all 4 of these in under a half hour. " +
+                " It’s a great holiday craft to do with the kids!\n" +
+                "\n" +
+                "Have fun!\n" +
+                "\n";
+        decoration_information_5.color = Color.rgb(240, 280, 255);
+        decoration_information_5.image = "decoration_5";
+        decoration_information_5.URL = "http://tidymom.net/2011/waterless-snow-globes-tutorial/";
+
+        items.add(decoration_information_5);
+
+        Information decoration_information_6 = new Information();
+        decoration_information_6.name = "Hanging Christmas Lights ";
+        decoration_information_6.description = "A house with Christmas lights is " +
+                "beautiful, but it can be dangerous to achieve it. Be careful!";
+        decoration_information_6.color = Color.rgb(70, 280, 255);
+        decoration_information_6.image = "decoration_6";
+        decoration_information_6.URL = "http://www.fromcalculustocupcakes.com/hanging-christmas-lights-easy-way/";
+
+        items.add(decoration_information_6);
+
+        return items;
+    }
+
+
+
+}
