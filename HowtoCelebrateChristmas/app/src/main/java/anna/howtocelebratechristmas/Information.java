a836aba99535afc85f05d791843441979b7df309
Anna the Hybrid <skateboardingalice@gmail.com>
2015-12-07 13:46:03 -0600

    Revert "play store"
    This reverts commit f1e673c542ea2c5678c319910f0a4aeca7228958.

--- HowtoCelebrateChristmas/app/src/main/java/anna/howtocelebratechristmas/Information.java
+++
@@ -1,9 +0,0 @@
-package anna.howtocelebratechristmas;
-
-public class Information {
-    public String name;
-    public String description;
-    public int color;
-    public String image;
-    public String URL;
-}
